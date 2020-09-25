158612b82d8242c59f23b7339a9b27e4f8e9de13
Brian-Muchera <mucherabrian2@gmail.com>
2020-09-25 13:23:19 +0300

    changes

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 export MAIL_USERNAME ='brianmuchera4@gmail.com'
 export  MAIL_PASSWORD ='shitakwaaa8*'
 export SECRET_KEY='0443'
-export SQLALCHEMY_DATABASE_URI='DEV_DATABASE_URL'
-python3.8 manage.py server
+python manage.py server
