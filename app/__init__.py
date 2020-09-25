4361518b6601379402a2ba56aac7a01c968e7ecb
Brian-Muchera <mucherabrian2@gmail.com>
2020-09-25 12:04:18 +0300

    made changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,22 +3,26 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
+from config import config_options
 
-from config import Config
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-bootstrap = Bootstrap(app)
-mail = Mail(app)
+db = SQLAlchemy()
+login_manager = LoginManager()
+bootstrap = Bootstrap()
+mail = Mail()
 login_manager.login_view = 'auth.login'
 login_manager.session_protection = 'strong'
 
 
-def create_app():
-    app.config.from_object(Config)
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config_options[config_name])
     from .auth import auth as auth_blueprint
     from .main import main as main_blueprint
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(main_blueprint)
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
     return app
+
--- config.py
+++ config.py
@@ -21,6 +21,33 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://muchera-brian:brian@localhost/pitch'
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://muchera-brian:brian@localhost/pitch'
+    DEBUG = True
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}
 
 
 
